f6ad30e374844ed272affa7d2c2b55223f0919ef
Giuseppe M Pereira <giuseppe.pereira@meta.com.br>
2019-11-14 15:38:59 -0300

    Calculator Funcionalidade:Somar

--- src/calculator/__tests__/index.spec.js
+++ src/calculator/__tests__/index.spec.js
@@ -1,6 +1,35 @@
 const calculadora = require('../')
-describe('Testando calculator', () => {
-  it('soma', done => {
-    expect(calculadora.somar(2, 2)).toBe(4)
+
+// # Think
+describe('Módulo: [ calculator ]', () => {
+  describe('Funcionalidade: [ somar ]', () => {
+    it('Cenario 1: 2 + 2 = 4', done => {
+      expect(calculadora.somar(2, 2)).toBe(4)
+      done()
+    })
+    it('Cenario 2: 3 + 2 = 5', done => {
+      expect(calculadora.somar(3, 2)).toBe(5)
+      done()
+    })
+    it('Cenario 3: -3 + 2 = -1', done => {
+      expect(calculadora.somar(-3, 2)).toBe(-1)
+      done()
+    })
+    it('Cenario 4: -3 + -2 = -5', done => {
+      expect(calculadora.somar(-3, -2)).toBe(-5)
+      done()
+    })
+    it('Cenario 5: 3.33 + 3.33 = 6.66', done => {
+      expect(calculadora.somar(3.33, 3.33)).toBe(6.66)
+      done()
+    })
+    it('Cenario 6: -3.33 + 3.33 = 0', done => {
+      expect(calculadora.somar(3.33, -3.33)).toBe(0)
+      done()
+    })
+    it('Cenario 6: -3.331 + 3.33 = 0', done => {
+      expect(calculadora.somar(3.331, -3.33)).toBe(0.01)
+      done()
+    })
   })
 })
---
+++ src/calculator/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  somar: (a, b) => {
+    return a + b
+  }
+}
