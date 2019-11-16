2c8daafe0da4c3a6d8f44a942149b0bc43d62613
Giuseppe M Pereira <giuseppe.pereira@meta.com.br>
2019-11-16 20:36:38 -0300

    Calculator division: dois números inteiros, ambos negativos:refactor

--- src/calculator/__tests__/calculator.division.test.js
+++ src/calculator/__tests__/calculator.division.test.js
@@ -12,11 +12,18 @@ describe('[ Calculator ]', function() {
     it('dois números inteiros', async () => {
       expect.hasAssertions()
       await new Promise(done => {
-        expect(division(2, 2)).toBe(1)
+        expect(division(4, 2)).toBe(2)
         done()
       })
     })
-    it('dois números inteiros, um negativo', async () => {
+    it('dois números inteiros, primeiro negativo', async () => {
+      expect.hasAssertions()
+      await new Promise(done => {
+        expect(division(-2, 2)).toBe(-1)
+        done()
+      })
+    })
+    it('dois números inteiros, segundo negativo', async () => {
       expect.hasAssertions()
       await new Promise(done => {
         expect(division(2, -2)).toBe(-1)
