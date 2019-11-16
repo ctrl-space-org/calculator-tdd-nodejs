5dffd13b9020ae68ff13888bede6d134d2197d6c
Giuseppe M Pereira <giuseppe.pereira@meta.com.br>
2019-11-16 20:34:29 -0300

    Calculator division: dois números inteiros

---
+++ src/calculator/__tests__/calculator.division.test.js
@@ -0,0 +1,20 @@
+const { division } = require('../')
+
+describe('[ Calculator ]', function() {
+  it('divisio method exists', async () => {
+    expect.hasAssertions()
+    await new Promise(done => {
+      expect(division).toBeDefined()
+      done()
+    })
+  })
+  describe('[ division method ]', function() {
+    it('dois números inteiros', async () => {
+      expect.hasAssertions()
+      await new Promise(done => {
+        expect(division(2, 2)).toBe(1)
+        done()
+      })
+    })
+  })
+})
--- src/calculator/index.js
+++ src/calculator/index.js
@@ -9,7 +9,19 @@ const subtraction = (a = 0, b = 0) => a - b
  */
 const addition = (a = 0, b = 0) => a + b
 
+/**
+ * Método de divisão
+ *
+ * @param {number} a num1
+ * @param {number} b num2
+ * @returns {number} result
+ */
+const division = (a, b) => {
+  return a / b
+}
+
 module.exports = {
   addition,
-  subtraction
+  subtraction,
+  division
 }
