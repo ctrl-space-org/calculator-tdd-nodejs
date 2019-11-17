afb99e5bac1a2bb989dc74bb8c493397bce09f05
Giuseppe M Pereira <giuseppe.pereira@meta.com.br>
2019-11-16 21:38:45 -0300

    DDD refactor

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require('express')()
 
+app.post('/calculator/addition', function(req, res) {
+  res.send('POST request to the homepage')
+})
+
 app.listen(port => {
   console.log('Calculator service running.')
 })
