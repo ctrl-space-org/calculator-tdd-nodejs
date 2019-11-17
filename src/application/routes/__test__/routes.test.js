b2a1c2e5ac14c167f00dd26d8b1aeb34aef20203
Giuseppe M Pereira <giuseppe.pereira@meta.com.br>
2019-11-17 13:07:31 -0300

    refactor routes

--- src/application/routes/__test__/routes.test.js
+++ src/application/routes/__test__/routes.test.js
@@ -1,3 +1,15 @@
 const routes = require('..')
 
-describe('[ Testing Application Routes Summary ]', () => {})
+describe('[ Testing Application Routes Summary ]', () => {
+  it('route /status registrada.', async () => {
+    expect.hasAssertions()
+    await new Promise(done => {
+      expect(
+        routes.stack.filter(r => r.route).map(r => r.route.path)
+      ).toContain('/status')
+      done()
+    })
+  })
+
+  it.todo('route /calculator registrada.')
+})
